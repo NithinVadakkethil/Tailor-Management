8eb016b16cda9261144b3505a9a4a051af432207
Nithin <nithinvadakkethil18@gmail.com>
2026-06-22 09:05:56 -0700

    fix jest ci pipeline

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,12 +1,17 @@
-// jest.config.js
 module.exports = {
     testEnvironment: "node",
+
     verbose: true,
+
     setupFiles: [
         "<rootDir>/jest.setup.js",
     ],
-    setupFilesAfterEnv: [
-        "<rootDir>/tests/setup.js",
-    ],
+
+    globalSetup:
+        "<rootDir>/tests/globalSetup.js",
+
+    globalTeardown:
+        "<rootDir>/tests/globalTeardown.js",
+
     testTimeout: 30000,
 };
